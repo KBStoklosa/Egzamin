c2563f2c8b48ea643c48114f915f2e4b00dec165
ksl <stoklosakb@gmail.com>
2025-11-19 13:21:30 +0100

    test:add dotenv

--- tests/logowanie.spec.js
+++ tests/logowanie.spec.js
@@ -1,12 +1,25 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
+import { beforeEach } from "node:test";
 
-test("logowanie", async ({ page }) => {
-
+test. beforeEach(async ({ page }) => {
     await page.goto ('/');
 
-    await page.getByTestId('login-username').fill('admin'); 
-    await page.getByTestId('login-password').fill('admin'); 
+});
+
+
+test("logowanie admin", async ({ page }) => {
+
+    await page.getByTestId('login-username').fill(process.env.LOGIN_ADMIN); 
+    await page.getByTestId('login-password').fill(process.env.PASSWORD_ADMIN); 
     await page.locator('#login-button').click(); 
     await expect(page.getByText ('Zalogowano jako admin')).toBeVisible();
+});
+
+test("logowanie user", async ({ page }) => {
+
+    await page.getByTestId('login-username').fill(process.env.LOGIN_USER); 
+    await page.getByTestId('login-password').fill(process.env.PASSWORD_USER); 
+    await page.locator('#login-button').click(); 
+    await expect(page.getByText ('Witaj: user')).toBeVisible();
 });
